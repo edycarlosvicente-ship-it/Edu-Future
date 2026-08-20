a8b64dd8a42e37cfe11a019c0883989408d5fc76
edycarlosvicente-ship-it <edycarlosvicente@gmail.com>
2026-08-20 14:43:45 +0200

    update app.py : erro na linha 13

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#adicionar rota para home page
+
 
 @app.route("/")
 def index():
   return render_template("index.html")
 
-#rodar a app
+#coreção de erro na linha 13 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", host=5000)
+  app.run(host="0.0.0.0", port=5000)
